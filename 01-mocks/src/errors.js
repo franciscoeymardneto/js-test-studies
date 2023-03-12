f2499ae0fcf805ff54eeda84ccba13e77b8f4bdd
eymard <francisco.eymard.oliveira@gmail.com>
2023-03-12 12:16:13 -0300

    implement validstion csv function

---
+++ 01-mocks/src/errors.js
@@ -0,0 +1,6 @@
+const errors = {
+    FILE_LENGTH_ERROR_MESSAGE: 'The content lenght is invalid',
+    FILE_FIELDS_ERROR_MESSAGE: 'The provided properties are invalid'
+}
+
+module.exports = errors
--- 01-mocks/src/file.js
+++ 01-mocks/src/file.js
@@ -1,19 +1,48 @@
 const { readFile } = require("node:fs/promises")
 const { join } = require("node:path")
+const errorMessages = require("./errors")
 
+const DEFAULT_OPTIONS = {
+    maxLine: 3,
+    fields: ["id","name","profession","age"]
+}
 class File{
     static async csvToJson(filePath){
-        return `Hello, ${await this.getFileContent(filePath)}`
+        const content = await File.getFileContent(filePath)
+        const validation = File.isValid(content)
+        if (!validation.valid) throw new Error(validation.error)
+        
+        return content
     }
     static async getFileContent(filePath){
         const fileName = join(__dirname,filePath)
         return (await readFile(fileName)).toString('utf-8')
     }
+    static isValid (csvString, options = DEFAULT_OPTIONS) {
+        const [header, ...content] = csvString.split('\n')
+        const isHeaderValid = header === options.fields.join(',')
+
+        if (!isHeaderValid) {
+            return {
+                error: errorMessages.FILE_FIELDS_ERROR_MESSAGE,
+                valid: false
+            }
+            
+        }
+
+        if (content.length > options.maxLine) {
+            return {
+                error: errorMessages.FILE_LENGTH_ERROR_MESSAGE,
+                valid: false
+            }
+        }
+    }
 }
 
 (async ()=> {
-    const result = await File.csvToJson('./../mocks/threeItems-valid.csv')
-    console.log(result)
+    // const result = await File.csvToJson('./../mocks/threeItems-valid.csv')
+    const result = await File.csvToJson('./../mocks/header-invalid.csv')
+    //console.log(result)
 
 })()
 
