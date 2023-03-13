eeb20b18139a6f7b0503369f866fce4856c6068b
eymard <francisco.eymard.oliveira@gmail.com>
2023-03-13 20:59:41 -0300

    implement test for default route

--- 04-coverage-e2e/src/api.test.js
+++ 04-coverage-e2e/src/api.test.js
@@ -6,14 +6,29 @@ const app = require('./api')
 describe('API test suite', () => {
     describe('/contact', () => {
         it('should request the contact page and return HTTP status 200', async () => {
-            const response = await request(app)
-                                            .get('/contact')
-                                            .expect(200)
+            await request(app)
+                .get('/contact')
+                .expect(200)
         })
         it('should request the contact page and the the response must have "Contact Us page!"', async () => {
             const response = await request(app)
-                                            .get('/contact')
-            assert.deepStrictEqual(response.text,'Contact Us page!')
+                .get('/contact')
+            assert.deepStrictEqual(response.text, 'Contact Us page!')
         })
     })
+
+    describe('/hello', () => {
+        it('should request in an inexistent route /hi redirect to /hello and return HTTP status 200', async () => {
+            await request(app)
+                .get('/hi')
+                .expect(200)
+        })
+        it('should request the defsult page and the the response must have "Hello world!"', async () => {
+            const response = await request(app)
+                .get('/hi')
+            assert.deepStrictEqual(response.text, 'Hello world!')
+        })
+    })
+
+
 })
