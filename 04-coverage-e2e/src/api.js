bd03c93715cc645123a77384de1630b1722fd8fa
eymard <francisco.eymard.oliveira@gmail.com>
2023-03-13 23:35:10 -0300

    implement login method

--- 04-coverage-e2e/src/api.js
+++ 04-coverage-e2e/src/api.js
@@ -1,10 +1,30 @@
 const http = require('http')
 
-
+const DEFAULT_USER = {'user': 'Eymard', 'pass': '12345'}
 const routes = {
     '/contact:get': (request, response) => {
         response.write('Contact Us page!')
         return response.end()
+    },
+
+    '/login:post': async (request, response) => {
+        // request é um iterator
+        for await (const data of request) {
+            const user = JSON.parse(data)
+
+            if (
+                user.user !== DEFAULT_USER.user ||
+                user.pass !== DEFAULT_USER.pass
+            ) {
+                response.writeHead(401)
+                response.write('Access Denied!')
+                return response.end()
+            } {
+                response.write(`Login successfully, Welcome ${user.user}!`)
+                return response.end()
+            }
+        }
+
     },
 
     default: (request, response) => {
