ac31deaec0e34c7e8064374d86a77bc4962f4641
eymard <francisco.eymard.oliveira@gmail.com>
2023-03-13 23:46:39 -0300

    test: implement login tests

--- 04-coverage-e2e/src/api.test.js
+++ 04-coverage-e2e/src/api.test.js
@@ -23,10 +23,41 @@ describe('API test suite', () => {
                 .get('/hi')
                 .expect(200)
         })
-        it('should request the defsult page and the the response must have "Hello world!"', async () => {
+        it('should request the default page and the the response must have "Hello world!"', async () => {
             const response = await request(app)
                 .get('/hi')
             assert.deepStrictEqual(response.text, 'Hello world!')
+        })
+    })
+
+    describe('/login', () => {
+        it('should request login and return 401', async () => {
+            await request(app)
+            .post('/login')
+            .send({'user': 'John', 'pass': '12345'})
+            .expect(401)
+        })
+        it('should request login and response text must be "Access Denied!"', async () => {
+            const response = await request(app)
+            .post('/login')
+            .send({'user': 'John', 'pass': '12345'})
+
+            assert.deepStrictEqual(response.text, 'Access Denied!')
+        })
+
+        it('should request login and return 200"', async () => {
+            await request(app)
+            .post('/login')
+            .send({'user': 'Eymard', 'pass': '12345'})
+            .expect(200)
+        })
+
+        it('should request login and get logged successfully and the resonse text must countains the username', async () => {
+            const response = await request(app)
+            .post('/login')
+            .send({'user': 'Eymard', 'pass': '12345'})
+
+            assert.match(response.text, /Eymard/)
         })
     })
 
